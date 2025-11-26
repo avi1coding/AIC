eeb33db4b2243cd5df5e124bf87add8762a65a0f
Avi Mehta <avimehta129@gmail.com>
2025-11-26 03:10:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2998894426-c274f",
+  "appId": "1:432637135308:web:603dd81a1659309786b569",
+  "apiKey": "AIzaSyA2i5WxLoMKX78UYTcpxSLd6IRcJ1EBu00",
+  "authDomain": "studio-2998894426-c274f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "432637135308"
+};
